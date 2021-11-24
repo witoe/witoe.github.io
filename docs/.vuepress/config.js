f32036003c8338c8a2f76c5b4a9c99e5bc2f96ff
guyong <jcel@qq.com>
2021-11-24 15:51:26 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    lang: 'en-US',
+    title: 'IGUU',
+    description: 'MY personal blog.',
+
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png',
+    },
+}
